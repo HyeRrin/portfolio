42292d4564b7c8996010dcb93ee6e97413f1e80d
Hyerin Joo <candy980426@gmail.com>
2022-07-27 21:16:11 +0900

    ✨ 타이틀 타이핑 효과 추가

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// 맨 위로 이동 버튼
 let moveToTop = function () {
   document.body.scrollIntoView({ behavior: "smooth" });
 };
+
+// 타이핑 효과
+const content = "안녕하세요 :)\n개발꿈나무 주혜린입니다.";
+const txt = document.querySelector(".txt-title");
+let index = 0;
+
+let typing = function () {
+  let tmp = content[index++];
+  txt.innerHTML += tmp === "\n" ? "<br />" : tmp;
+  if (index > content.length) {
+    txt.textContent = "";
+    index = 0;
+  }
+};
+
+setInterval(typing, 200);
